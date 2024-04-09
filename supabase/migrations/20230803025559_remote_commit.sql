93002484415ace138f41bd8bf27e7d32bc85e6ee
Han Qiao <qiao@supabase.io>
2024-04-09 11:39:50 +0800

    fix: switch off pgaudit before enabling postgis (#10)

--- supabase/migrations/20230803025559_remote_commit.sql
+++ supabase/migrations/20230803025559_remote_commit.sql
@@ -9,8 +9,11 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+SET pgaudit.log = 'none';
 
 CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
 
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
 
